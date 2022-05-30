71021ec1d9d8e395f5bd144e0c971bf4406b165e
Joao Oliveira <thxjao.dev@gmail.com>
2022-05-30 18:54:57 -0300

    feat: to weird case

---
+++ 6kyu/toWeirdCase.js
@@ -0,0 +1,23 @@
+function toWeirdCase(string){
+
+    const dividedWords = string.split(' ');
+
+    const formattedWords = dividedWords.map((word) => {
+        return word.split('').map((letter, index)  => {
+            if (++index % 2 === 0) {
+                return letter.toLowerCase()
+            } else {
+                return letter.toUpperCase()
+            }
+        }).join('')
+    })
+    
+    const unifiedWords = formattedWords.join(' ')
+
+    return unifiedWords
+}
+
+console.log(toWeirdCase("Weird string case"))
+
+
+
